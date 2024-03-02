c9ae6bdcbb7c753b434498b6d3e19f367a340d4b
Boris Bredikhin <boris00043@yandex.ru>
2024-03-02 18:23:11 +0500

    add schemas

--- Multi-lingual Phoneme Recognition/backend_for_prod/src/app.py
+++ Multi-lingual Phoneme Recognition/backend_for_prod/src/app.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
 from ai_utils import Model, SpeechDataset
+from schemas import PredictionSchema
 from utils import get_audio, to_str, webm_to_wav
 
 app = FastAPI()
@@ -23,7 +24,7 @@ app.add_middleware(
 model = Model()
 dataset = SpeechDataset()
 
-@app.post('/predict')
+@app.post('/predict', response_model=PredictionSchema)
 async def predict(file: Annotated[UploadFile, File(...)]):
     request_id = secrets.token_urlsafe(8)
     out_path = f'../data/{request_id}.webm'
