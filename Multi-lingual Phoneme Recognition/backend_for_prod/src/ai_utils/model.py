dd1f5f52a87f34ca1dbd96ffa75bff427475ccb6
Boris Bredikhin <boris00043@yandex.ru>
2024-03-03 14:58:16 +0500

    testing individual models

--- Multi-lingual Phoneme Recognition/backend_for_prod/src/ai_utils/model.py
+++ Multi-lingual Phoneme Recognition/backend_for_prod/src/ai_utils/model.py
@@ -1,7 +1,12 @@
+from collections import deque
+
 from torch import nn
+from torch.utils.data import DataLoader
+import torch
 import torchaudio
 
-from .dataset import padded_stack
+from .dataset import SpeechDataset, padded_stack
+from utils import get_y_lengths, str_to_tensor
 
 train_transforms = torchaudio.transforms.MelSpectrogram(n_mels=128)
 
@@ -13,19 +18,74 @@ class Model(nn.Module):
         self.rnn = nn.GRU(128, 256, 1, batch_first=True, bidirectional=True)
         self.fc = nn.Linear(256 * 2, 34)
         self.act = nn.LogSoftmax(-1)
-    
+
     def forward(self, x):
         # X: batch x 128 x len
         x = x[:, :, ::4]
-        x = x.permute(0,2,1)
+        x = x.permute(0, 2, 1)
         x, _ = self.rnn(x)
         x = self.fc(x)
         x = self.act(x)
         return x
-    
+
     def predict(self, x):
-        spectrogram = [train_transforms(x).permute(0, 2, 1)]
+        if isinstance(x, list):
+            spectrogram = [train_transforms(i).permute(0, 2, 1) for i in x]
+        else:
+            spectrogram = [train_transforms(x).permute(0, 2, 1)]
         spectrogram = padded_stack(spectrogram).permute(0, 2, 1)
         output = self(spectrogram)
-        return output.argmax(-1)
- 
+        return output
+
+    def train_on_data(
+        self,
+        dataset: SpeechDataset,
+        /,
+        epochs: int | None = None,
+        loss_delta: float | None = 0.01,
+        cer_delta: float | None = 0.01,
+        batches_per_step: int = 1,
+        plateu_length: int = 5,
+    ):
+        '''
+        cer_delta: пока игнорируется
+        '''
+
+        dataloader = DataLoader(
+            dataset, batch_size=4, shuffle=True, collate_fn=SpeechDataset.collate
+        )
+        last_losses = deque(maxlen=plateu_length)
+
+        self.train()
+
+        loss_function = nn.CTCLoss(zero_infinity=True)
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+
+        optimizer.zero_grad()
+
+        epoch = 0
+
+        while True:
+            epoch += 1
+            if epochs is not None and epoch > epochs:
+                break
+
+            for bn, batch in enumerate(dataloader):
+                output = self.predict(batch[0])
+                target_t, target_len = str_to_tensor(batch[1])
+                loss = loss_function(
+                    output.permute(1, 0, 2), target_t, get_y_lengths(output), target_len
+                )
+                loss.backward()
+
+                last_losses.append(loss.item())
+
+                if bn % batches_per_step == 0:
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+            if (
+                len(last_losses) == plateu_length
+                and abs(last_losses[-1] - sum(last_losses) / len(last_losses)) < loss_delta
+            ):
+                break
--- Multi-lingual Phoneme Recognition/backend_for_prod/src/app.py
+++ Multi-lingual Phoneme Recognition/backend_for_prod/src/app.py
@@ -6,37 +6,55 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
 from ai_utils import Model, SpeechDataset
-from schemas import PredictionSchema
-from utils import get_audio, to_str, webm_to_wav
+from schemas import LabelingRequestSchema, PredictionSchema
+from utils import clear_str, get_audio, to_str, webm_to_wav
 
 app = FastAPI()
 
-origins = ['*']
+data_dir = os.path.join(os.path.dirname(__file__), 'data')
+if  not os.path.exists(data_dir):
+    os.mkdir(data_dir)
+
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-     allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 model = Model()
+model.eval()
 dataset = SpeechDataset()
 
-@app.post('/predict', response_model=PredictionSchema)
+
+@app.post("/predict", response_model=PredictionSchema)
 async def predict(file: Annotated[UploadFile, File(...)]):
     request_id = secrets.token_urlsafe(8)
-    out_path = f'../data/{request_id}.webm'
-    with open(out_path, 'wb') as f:
+    out_path = f"{data_dir}/{request_id}.webm"
+    with open(out_path, "wb") as f:
         f.write(await file.read())
-    
-    webm_to_wav(out_path, out_path+'.wav')
+
+    webm_to_wav(out_path, out_path + ".wav")
 
     os.remove(out_path)
 
-    audio = get_audio(out_path+'.wav')
+    audio = get_audio(out_path + ".wav")
 
-    result = model.predict(audio)[0]
+    result = model.predict(audio).argmax(-1)[0]
 
-    return {'result': to_str(result), 'request_id': request_id}
+    return {"result": to_str(result), "request_id": request_id}
+
+
+@app.post("/label")
+async def label(data: LabelingRequestSchema):
+    dataset.add_audio(f"{data_dir}/{data.request_id}.webm.wav", clear_str(data.label))
+    return {"status": "ok"}
+
+
+@app.post("/train")
+async def train():
+    model.train_on_data(dataset, epochs=100)
+    return {"status": "ok"}
