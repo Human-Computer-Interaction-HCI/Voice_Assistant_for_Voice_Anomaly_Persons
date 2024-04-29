a6b163d88cbd978e4e5b123c024af2a02c4e5bfd
Бредихин Борис Андреевич <boris00043@yandex.ru>
2024-04-29 11:54:22 +0500

    show model metrics during training

--- Multi-lingual Phoneme Recognition/frontend_for_prod/src/app/inner/model/actions.ts
+++ Multi-lingual Phoneme Recognition/frontend_for_prod/src/app/inner/model/actions.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { getModelInfo as _getModelInfo, ModelInfo, trainModel as _trainModel, ModelTrainInfo } from "@/api/model";
+import { getModelInfo as _getModelInfo, ModelInfo, trainModel as _trainModel, ModelTrainInfo, getModelMetrics as _getModelMetrics } from "@/api/model";
 
 export async function getModelInfo(): Promise<ModelInfo> {
     return await _getModelInfo();
@@ -8,4 +8,8 @@ export async function getModelInfo(): Promise<ModelInfo> {
 
 export async function trainModel(): Promise<ModelTrainInfo> {
     return await _trainModel();
+}
+
+export async function getModelMetrics(task_id: string): Promise<any> {
+    return await _getModelMetrics(task_id);
 }
--- Multi-lingual Phoneme Recognition/frontend_for_prod/src/app/inner/model/page.tsx
+++ Multi-lingual Phoneme Recognition/frontend_for_prod/src/app/inner/model/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Typography from "@mui/material/Typography";
-import { getModelInfo, trainModel } from "./actions"
+import { getModelInfo, trainModel, getModelMetrics } from "./actions"
 import { useEffect, useState } from "react";
 import { ModelInfo } from "@/api/model";
 import Card from "@mui/material/Card";
@@ -8,25 +8,32 @@ import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import { Button } from "@mui/material";
+import MetricPlot from "@/components/MetricPlot";
 
 export default function Page() {
     const [modelInfo, setModelInfo] = useState<ModelInfo>()
-    const [taskId, setTaskId] = useState<string>()
+    const [taskId, setTaskId] = useState<string>("RKRO5Noz9cM")
+    const [training, setTraining] = useState(false)
 
     useEffect(() => {
         getModelInfo().then(setModelInfo)
     }, [])
 
+    function startTrain() {
+        trainModel().then(r=>setTaskId(r.task_id))
+        setTraining(true)
+    }
+
     return <>
         <Typography variant="h1">Моя модель</Typography>
-        <Card sx={{ width: "fit-content", maxWidth: "300px" }}>
+        <Card sx={{ width: "fit-content" }}>
             <CardHeader title={`Модель ${modelInfo?.id}`} />
             <CardContent>
                 <Typography>Метрики</Typography>
-                {taskId&&<p><b>TODO:</b> График {taskId}</p>}
+                {taskId&&<MetricPlot taskId={taskId} refresh={()=>getModelMetrics(taskId)} />}
             </CardContent>
             <CardActions>
-                <Button size="small" onClick={()=>trainModel().then(r=>setTaskId(r.task_id))}>Запустить обучение</Button>
+                {!training&&<Button size="small" onClick={startTrain}>Запустить обучение</Button>}
             </CardActions>
         </Card>
     </>
